9f9c3a0aa98a169f894dbce9f44909fc525bde57
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 15:23:54 +0800

    Print output

--- rjust_alternative.py
+++ rjust_alternative.py
@@ -3,4 +3,7 @@ user_input = input("Enter a string: ")
 # ask user to enter width to adjust
 width = int(input("Enter the total width: "))
 # adjust the statement based on the width entered
+if len(user_input) < width:
+    user_input = " " * (width - len(user_input)) + user_input
 # print output
+print(f"Statement working same as rjust function: '{user_input}'")
