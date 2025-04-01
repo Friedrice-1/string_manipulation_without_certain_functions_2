06e5c7c6a5e408270f1b285225e403e744edbf1a
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 14:29:36 +0800

    Prints output

--- removesuffix_alternative.py
+++ removesuffix_alternative.py
@@ -1,6 +1,9 @@
 # ask user to input a word
 user_input = input("Please enter a word: ")
 # ask user for a suffix to remove
+suffix = input("Enter a suffix to remove: ")
 # remove suffix
+if user_input.endswith(suffix):
+    new_user_input = user_input[:-len(suffix)]
 # print output
-print(user_input)
+print(new_user_input)
