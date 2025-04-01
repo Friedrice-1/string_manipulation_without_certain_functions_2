1b57131f20f5e5b2c77bc5241706d9c7deb29f58
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 15:43:23 +0800

    Prints result

--- index_alternative.py
+++ index_alternative.py
@@ -3,4 +3,13 @@ user_input = input("Enter a string: ")
 # ask user to enter a character to find
 find = input("Enter the character you want to find: ")
 # find the first occurrence of the character
+position = -1
+for i in range(len(user_input)):
+    if user_input[i] == find:
+        position = i
+        break
 # print output
+if position != -1:
+    print(f"The first occurrence of '{find}' is at index {position}")
+else:
+    print(f"'{find}' not found in the string")
