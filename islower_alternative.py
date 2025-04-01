46046d19a8191799e5a2659145de4ffe1d9677d6
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 14:39:12 +0800

    Converts user input into lower case

--- islower_alternative.py
+++ islower_alternative.py
@@ -1,5 +1,11 @@
 # ask user to input a statement
 user_input = input("Please enter a statement: ")
 # convert the statement into lowercase
+lowercase_user_input = ""
+for char in user_input:
+    if 'A' <= char <= 'Z':
+        lowercase_user_input += chr(ord(char) + 32)
+    else:
+        lowercase_user_input += char
 # check if the statement is the same as the converted one
 # print result
