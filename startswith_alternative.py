280c6891d9c2464ae9dc22ad2515912977ee8d2a
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 14:43:33 +0800

    Print result

--- startswith_alternative.py
+++ startswith_alternative.py
@@ -3,4 +3,9 @@ user_input = input("Please enter an input: ")
 # ask user to check what the statement starts with
 starts_with = input("Enter to check if the statements starts with it: ")
 # checks if it starts with the input entered
+if starts_with == user_input[:len(starts_with)]:
 # print result
+    print(f"It starts with {starts_with}")
+else:
+# print result
+    print(f"It does not starts with {starts_with}")
