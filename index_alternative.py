33e8b527644dd849b9bc826e8e80dbcfd10fb1e3
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 15:41:07 +0800

    User inputs

--- index_alternative.py
+++ index_alternative.py
@@ -1,4 +1,6 @@
 # ask user to enter a statement
+user_input = input("Enter a string: ")
 # ask user to enter a character to find
+find = input("Enter the character you want to find: ")
 # find the first occurrence of the character
 # print output
