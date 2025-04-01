79adbd16cdbcd0c8199d62ce5e71d34d1ba7f59b
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 15:47:37 +0800

    Prints result

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -3,4 +3,13 @@ user_input = input("Enter a statement: ")
 # ask user to enter a character to find
 find = input("Enter the character you want to find: ")
 # find the last occurrence of the character
+position = -1
+for i in range(len(user_input) -1, -1, -1):
+    if user_input[i] == find:
+        position = i
+        break
 # print output
+if position != -1:
+    print(f"The last occurrence of '{find}' is at index {position}")
+else:
+    print(f"'{find}' not found in the string")
