b48608f0dc109587609917d5c2ca430674489500
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 14:38:42 +0800

    User input

--- islower_alternative.py
+++ islower_alternative.py
@@ -1,4 +1,5 @@
 # ask user to input a statement
+user_input = input("Please enter a statement: ")
 # convert the statement into lowercase
 # check if the statement is the same as the converted one
 # print result
