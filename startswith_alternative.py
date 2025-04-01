073da3a7b4294886b2e40fbc62128d6d3092d333
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 14:42:42 +0800

    User inputs

--- startswith_alternative.py
+++ startswith_alternative.py
@@ -1,4 +1,6 @@
 # ask user to input a statement
+user_input = input("Please enter an input: ")
 # ask user to check what the statement starts with
+starts_with = input("Enter to check if the statements starts with it: ")
 # checks if it starts with the input entered
 # print result
