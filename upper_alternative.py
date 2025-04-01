66ed05fc5bc449e81b8420abb46eab14205f160d
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 14:34:19 +0800

    Prints output

--- upper_alternative.py
+++ upper_alternative.py
@@ -1,5 +1,11 @@
 # ask user to input a statement
 user_input = input("Please enter a statement: ")
 # convert each character from string to upper
+uppercase_user_input = ""
+for char in user_input:
+    if 'a' <= char <= 'a':
+        uppercase_user_input += chr(ord(char) - 32)
+    else:
+        uppercase_user_input += char
 # print output
-print(user_input)
+print(uppercase_user_input)
