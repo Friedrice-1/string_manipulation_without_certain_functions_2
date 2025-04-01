91d5d1b39e68a30d9f0f707768c0e00d3707d4f9
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 14:20:17 +0800

    User input

--- rstrip_alternative.py
+++ rstrip_alternative.py
@@ -1,4 +1,6 @@
 # ask user to input a statement
+user_input = input("Please enter a statement: ")
 # find the last non space character
 # create the new string without the spaces at the right side
 # print output
+print(f"'{user_input}'")
