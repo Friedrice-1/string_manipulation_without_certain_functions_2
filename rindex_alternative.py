6f5c1e9c7b183b409315785b76c5dced07ed0bd9
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 15:47:06 +0800

    User inputs

--- rindex_alternative.py
+++ rindex_alternative.py
@@ -1,4 +1,6 @@
 # ask user to enter a statement
+user_input = input("Enter a statement: ")
 # ask user to enter a character to find
+find = input("Enter the character you want to find: ")
 # find the last occurrence of the character
 # print output
