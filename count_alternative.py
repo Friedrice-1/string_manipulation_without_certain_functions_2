7c441613a2102a9698bbd08579f0013063e2a7da
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 15:34:31 +0800

    User inputs

--- count_alternative.py
+++ count_alternative.py
@@ -1,4 +1,6 @@
 # ask user to input a statement
+user_input = input("Please enter a statement: ")
 # ask user to input the character they want to count
+find = input("Enter the character you want to count: ")
 # count the characters
 # print result
