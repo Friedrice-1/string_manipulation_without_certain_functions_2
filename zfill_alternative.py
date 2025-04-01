2369b2551730cad11f5bc38ad72942aa17fbb52c
Friedrice-1 <allenjayansing99@gmail.com>
2025-04-01 15:29:50 +0800

    Prints output

--- zfill_alternative.py
+++ zfill_alternative.py
@@ -3,4 +3,7 @@ user_input = input("Enter a number: ")
 # ask user to enter zeros to adjust
 width = int(input("Enter the the number of zeros: "))
 # adjust the statement based on the amount zeros entered
+if len(user_input) < width:
+    user_input = "0" * (width - len(user_input)) + user_input
 # print output
+print(user_input)
